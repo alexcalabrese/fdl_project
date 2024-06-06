ec0507de651c9283bd032f5302b38ab5a46569ad
Alex Calabrese <58480609+alexcalabrese@users.noreply.github.com>
2024-06-06 21:30:55 +0000

    Add explorative analisys

---
+++ classes/dataset_loader.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import os
+import json
+
+
+class DatasetLoader:
+
+    def __init__(self):
+        self.data = self.load_data()
+
+    def load_data(self, folder_name: str = "coco"):
+        print("Loading data..")
+        # Set the data folder
+        data_folder = os.path.join(folder_name)
+
+        # Create the directory if it doesn't exist
+        if not os.path.exists(data_folder):
+            os.makedirs(data_folder)
+
+        # Path to the captions JSON file
+        filename = os.path.join(data_folder, "annotations", "captions_train2014.json")
+
+        # Read and parse the JSON file
+        with open(filename, "r") as json_file:
+            data = json.load(json_file)
+
+        print("Data loaded succesfully!")
+
+        return data
+
+    def get_data_dict(self):
+        return self.data
+
+    def get_df_annotations(self):
+        return pd.DataFrame(self.data["annotations"])
+
+    def get_df_images(self):
+        return pd.DataFrame(self.data["images"])
